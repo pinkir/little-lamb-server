453e04956a76e060f0ba0cd93ead921ef96b032b
pinkir <pinki.roy@outlook.com>
2023-05-20 15:45:59 +0600

    toys data  added

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.og8u4je.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toyCollection = client.db('littleLamb').collection('tabToys');
+
+    app.get('/tabToys', async(req, res)=>{
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    // add a toy
+    app.post('/tabToys', async(req, res)=>{
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
